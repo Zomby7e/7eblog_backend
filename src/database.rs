cd4b288197e8cda22fdb4ba89e31b2be09a2adbd
zomby7e <zomby7e@gmail.com>
2022-08-28 03:37:25 +0800

    Added hashtag vector for read pagination.

--- src/database.rs
+++ src/database.rs
@@ -8,7 +8,14 @@ struct Read {
     title: String,
     content: String,
     timestamp: i64,
-    hashtag: String
+    hashtag: Vec<Hashtag>
+}
+
+#[derive(Serialize, Deserialize)]
+struct Hashtag{
+    id: i64,
+    name: String,
+    comment: String
 }
 
 // Initialize the local sqlite database.
@@ -18,7 +25,7 @@ pub fn database_init(){
 
     // Create "hashtags" database (If not exists).
     connection.execute(
-        "CREATE TABLE IF NOT EXISTS \"hashtags\" (
+        "CREATE TABLE IF NOT EXISTS \"hashtag\" (
         \"id\"	INTEGER NOT NULL UNIQUE,
         \"name\"	TEXT NOT NULL UNIQUE,
         \"comment\"	TEXT,
@@ -35,7 +42,7 @@ pub fn database_init(){
             \"timestamp\"	INTEGER,
             \"hashtag\"	TEXT,
             PRIMARY KEY(\"id\")
-        );", []
+            );", []
     ).unwrap();
 
     // Create "read" database (If not exists).
@@ -47,9 +54,19 @@ pub fn database_init(){
             \"timestamp\"	INTEGER,
             \"hashtag\"	TEXT,
             PRIMARY KEY(\"id\")
-        );", []
+            );", []
     ).unwrap();
 
+    // Create "read_hashtag" database (If not exists).
+    // This table is used to store the relationship between the hashtag table and the read table.
+    connection.execute(
+        "CREATE TABLE IF NOT EXISTS \"read_hashtag\" (
+            \"id\"	INTEGER NOT NULL UNIQUE,
+            \"read_id\"	TEXT NOT NULL,
+            \"hashtag_id\"	INTEGER NOT NULL,
+            PRIMARY KEY(\"id\" AUTOINCREMENT)
+            );", []
+    ).unwrap();
 }
 
 // get rows from "read" table.
@@ -66,7 +83,7 @@ pub fn database_get_read(query_object: QueryObject) -> Result<String, rusqlite::
                     title: row.get(1)?,
                     content: row.get(2)?,
                     timestamp: row.get(3)?,
-                    hashtag: row.get(4)?
+                    hashtag: database_get_read_hashtag(row.get(0)?)?
                 }
             )
         );
@@ -85,6 +102,38 @@ pub fn database_get_read(query_object: QueryObject) -> Result<String, rusqlite::
     Ok(read_str)
 }
 
+// 查某文章的所有的标签(待重构)
+fn database_get_read_hashtag (id: String) -> Result<Vec<Hashtag>, rusqlite::Error>{
+    let connection = Connection::open("blog_main.db")?;
+    let sql_string: String = String::from(format!(
+        "SELECT r.hashtag_id AS id, h.name, h.comment
+        FROM read_hashtag AS r JOIN hashtag AS h ON hashtag_id = h.id
+        WHERE read_id = \"{}\";", &id));
+    let mut stmt = connection.prepare(&sql_string)?;
+    let hashtags = stmt
+    .query_map([], |row|
+        Ok(
+            crate::database::Hashtag {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                comment: row.get(2)?
+            }
+        )
+    );
+    let mut hashtag_vec: Vec<Hashtag> = Vec::new();
+    for hashtag in hashtags? {
+        match hashtag {
+            Ok(hashtag_single) => {
+                hashtag_vec.push(hashtag_single)
+            },
+            Err(e) => {
+                return Err(e);
+            }
+        }
+    }
+    Ok(hashtag_vec)
+}
+
 // get rows from "read" table.
 pub fn database_get_read_pagination(query_object: ReadPaginationQueryObject) -> Result<String, rusqlite::Error> {
     let connection = Connection::open("blog_main.db")?;
@@ -99,7 +148,7 @@ pub fn database_get_read_pagination(query_object: ReadPaginationQueryObject) -> 
                     title: row.get(1)?,
                     content: row.get(2)?,
                     timestamp: row.get(3)?,
-                    hashtag: row.get(4)?
+                    hashtag: database_get_read_hashtag(row.get(0)?)?
                 }
             )
         );
@@ -114,7 +163,7 @@ pub fn database_get_read_pagination(query_object: ReadPaginationQueryObject) -> 
             }
         }
     }
-    let mut result_str = String::new();
-    result_str = serde_json::to_string(&read_vec).unwrap();
+    let result_str = serde_json::to_string(&read_vec).unwrap();
+    println!("---result_str---{}", &result_str);
     Ok(result_str)
 }
